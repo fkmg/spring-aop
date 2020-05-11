c6479207c34b86a35880f23a272c7423ed57652d
fkmg <2794934017@qq.com>
2020-05-11 22:40:55 +0800

    springaop练习

---
+++ src/main/java/com/sxt/service/impl/MyServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sxt.service.impl;
+
+import com.sxt.service.MyService;
+
+public class MyServiceImpl implements MyService {
+
+    public void login() {
+
+    }
+
+    public void testThrows(int flag) {
+
+    }
+}
